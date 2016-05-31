c2132a86a403a0b2d6c28a1591d48b94353c9971
BenjaminChauvet49FR <benjchauvet@gmail.com>
2016-05-31 11:43:20 +0200

    Modification de main

---
+++ src/main/java/mygreatproject/Main.java
@@ -0,0 +1,13 @@
+package mygreatproject;
+
+import com.google.common.base.Strings;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Strings.isNullOrEmpty("ok"));
+	}
+	
+	/*Modification sur myawesometesting*/
+
+}
